178896eb2985c94f99640e7a29866bf4ad5de4af
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2021-04-29 12:12:51 +0700

    Add class for creating bombs

---
+++ api/public/javascripts/Bomb.js
@@ -0,0 +1,54 @@
+// maximum_number_of_squares means total of squares on a board
+// Ex: The board has the size: 15x10 --> Total: 150 squares --> maximum_number_of_squares = 150
+// This is the class for basic creating bombs (just a GENERAL case: not calculate first click position of user)
+// The main class for creating bombs in this game is "BombExceptFirstClick", which extends from this class (Bomb)
+
+class Bomb {
+    constructor(number_of_bombs, number_of_squares_a_row, number_of_squares_a_column) {
+        this.number_of_bombs = parseInt(number_of_bombs);
+        this.maximum_number_of_squares = parseInt(number_of_squares_a_row) * parseInt(number_of_squares_a_column);
+        this.position_of_bombs = []; // Store position of the bombs
+
+
+        // The array will contain positions must be avoid when add to position_of_bombs
+        // Ex: The duplicate bombs, the first click of user, ...
+        this.exception = [];
+    }
+
+    // Return a random position
+    get_random_position() {
+        let min = 0;
+        let max = this.maximum_number_of_squares;
+        return Math.floor(Math.random() * max) + min; // Random from [0 to 80] (if the board is 9x9)
+    }
+
+
+
+
+    // Check if the position must not be add into position_of_bombs
+    is_position_excluded(position) {
+        for (let i = 0; i < this.exception.length; i++) {
+            if (position === this.exception[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    // Main function to create bombs' position
+    create_bombs() {
+        for (let i = 0; i < this.number_of_bombs; i++) {
+            let temp_position = this.get_random_position();
+
+            while (this.is_position_excluded(temp_position)) {
+                temp_position = this.get_random_position();
+            }
+
+            this.exception.push(temp_position);
+            this.position_of_bombs.push(temp_position);
+        }
+    }
+
+}
---
+++ api/public/javascripts/BombExceptFirstClick.js
@@ -0,0 +1,41 @@
+// When user click first at the board, we will create a chance for user to avoid clicking a bomb
+class BombExceptFirstClick extends Bomb {
+    constructor(first_click_position, number_of_bombs, number_of_squares_a_row, number_of_squares_a_column) {
+        super(number_of_bombs, number_of_squares_a_row, number_of_squares_a_column);
+        this.number_of_squares_a_row = parseInt(number_of_squares_a_row);
+        this.number_of_squares_a_column = parseInt(number_of_squares_a_column);
+        this.first_click_position = parseInt(first_click_position);
+    }
+
+
+    // Add 3x3 area (first click position is center) for exception
+    add_surrounding_first_click_into_exception() {
+        let relative_squares_exception =
+            new RelativeSquares(this.first_click_position, this.number_of_squares_a_row, this.number_of_squares_a_column);
+        relative_squares_exception = relative_squares_exception.get_3x3_area_position();
+        
+            for(let i=0; i<relative_squares_exception.length; i++){
+                this.exception.push(relative_squares_exception[i]);
+            }
+
+            console.log("Exception: " + this.exception);
+    }
+
+
+    // Override the main function
+    // Main function to create bombs' position
+    create_bombs() {
+        this.add_surrounding_first_click_into_exception();          // Override part
+
+        for (let i = 0; i < this.number_of_bombs; i++) {
+            let temp_position = this.get_random_position();
+
+            while (this.is_position_excluded(temp_position)) {
+                temp_position = this.get_random_position();
+            }
+
+            this.exception.push(temp_position);
+            this.position_of_bombs.push(temp_position);
+        }
+    }
+}
