5ed134fdb15dcf424afbe858303d1bc2ae40d395
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2021-05-18 20:31:35 +0700

    Create sample for ideas
    The file is ideas.js. The location is:
    public > javascripts > ideas.js

---
+++ webapp/public/javascripts/ideas.js
@@ -0,0 +1,13 @@
+// Any ideas should be written in this file
+
+
+// Open a square when user click on this square, left click
+function reveal_a_square(){
+
+}
+
+// Expand 3x3 squares when user use middle click on the square, the square clicked will be centered of 3x3 expansion
+function reveal_3x3_squares(){
+
+}
+
