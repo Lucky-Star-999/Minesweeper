e972a6d15ac9a837b1b01b31a806fb74ab6c6e21
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2021-05-18 22:22:38 +0700

    Add some comments

--- webapp/public/javascripts/ideas.js
+++ webapp/public/javascripts/ideas.js
@@ -1,5 +1,19 @@
 // Any ideas should be written in this file
 
+// The function help to show a square, or 3x3 squares by left click or right click is available in sandbox.html
+// Therefore, we do not need to create such functions
+
+/*
+    var board = [[0,1,2,2,1,0,0,0,1,1],[0,1,-1,-1,1,0,0,0,1,-1],
+    [0,1,2,3,2,1,0,0,1,1],[0,0,0,1,-1,2,2,1,1,0],[1,1,1,1,2,-1,3,-1,1,0],
+    [1,-1,1,0,1,2,-1,2,1,0],[1,2,2,2,1,2,1,1,0,0],[0,1,-1,2,-1,1,0,0,0,0],
+    [0,1,1,2,1,1,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
+
+    This is the sample board placed in sandbox.js. You can see the array if it can help you to imagine where the bombs are placed
+        , or whatever.
+*/
+
+//////////////////////////////////////// All sample functions here /////////////////////////////////////////////////////////
 
 // Open a square when user click on this square, left click
 function reveal_a_square(){
@@ -12,10 +26,34 @@ function reveal_3x3_squares(){
 }
 
 
+// Check if the game is win or lose, or playing ...
+// Default is playing..., neither win nor lose
+function is_won(){
+
+}
+
+function is_lose(){
+
+}
+
+// Check if the position contains a bomb, you can place this function in "is_lose()"
+function is_this_a_bomb(){
+
+}
 
 
 
-// ******************************** Main function here ********************************************
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// *********************************************** Main area here ********************************************
+// This main area like a main Class in java, just place any function here, and it will run
+// It likes
+// public static void main(String[] args){
+//          <You are here! (Main area)>            
+// }
+
 
 let position_arr = [1,2,3,11,12,13,21,22,23];
 
