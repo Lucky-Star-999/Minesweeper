6e977956b33959a12a6fb1e4145233621ff008f3
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2021-05-19 09:59:36 +0700

    Add some necessary sample functions

--- webapp/public/javascripts/ideas.js
+++ webapp/public/javascripts/ideas.js
@@ -15,16 +15,23 @@
 
 //////////////////////////////////////// All sample functions here /////////////////////////////////////////////////////////
 
+// Check if the square is set a flag on
+function is_flag_on(){
+
+}
+
 // Open a square when user click on this square, left click
 function reveal_a_square(){
 
 }
 
 // Expand 3x3 squares when user use middle click on the square, the square clicked will be centered of 3x3 expansion
+// However, when user use middle click, instead of expand all 3x3 squares, the square being flag on must be preserved!
+// Hint: You can use function is_flag_on() in this function to check what squares is set a flag on, 
+//          then except these squares
 function reveal_3x3_squares(){
 
 }
-
 
 // Check if the game is win or lose, or playing ...
 // Default is playing..., neither win nor lose
@@ -41,8 +48,31 @@ function is_this_a_bomb(){
 
 }
 
+// This function is to handle expansion effect for empty squares
+// The empty squares (number 0) should be automatically opened, but except the squares contains flag
+// Hint: You can use function is_flag_on() to check if the square considering is set a flag on
+// You can also use reveal_3x3_squares() to support this function
+function expand_all_empty_square(){
 
+}
 
+// This function helps to count the time ellapsed for playing the game. The time will be the score!
+function time_counter(){
+
+}
+
+// This function is used to turn the state back when user press "Undo"
+function undo(){
+
+}
+
+// Update state of the board to support for "undo" features
+// For example, there is a stack to store all the steps of playing for user
+// The function is to give all information (opened squares, flagged-on, ...) to the stack
+// Anytime user click undo, the stack will be popped
+function record_update_board(){
+
+}
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
