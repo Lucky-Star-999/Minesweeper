bfef77fc6d880febf643868ad44d0af98075999f
minhnguyenvinamr <ackar8985@gmail.com>
2021-05-26 13:24:46 +0700

    Functionalities for Settings screen and Buttons

---
+++ webapp/public/javascripts/game.js
@@ -0,0 +1,7 @@
+function exit() {
+    window.location.href = "../views/index.html"
+}
+
+var info_playername = document.getElementById('final-playername');
+var info_boardsize = document.getElementById('final-boardsize');
+var info_numberofbombs = document.getElementById('final-numberofbombs');
