1e37da13bb2508ecc79cb450419435cd9ced2078
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2021-05-29 12:00:12 +0700

    Fix some bugs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 
 
 function redirectSettings() {
-    alert("API ngoài cái màu mè ra thì chả được cái gì trong project này đâu!\n\n" +
+    alert(
     "Hiện tại project lấy board từ API (tui tạo ra). Nhưng vì khi đặt API lên server thật, nó ngăn các request lạ. " + 
     "Do đó cần bấm vào link phía dưới để open request tạm thời. Cứ cách nửa ngày vô link đó bấm 1 lần." +
     "\n\nTôi sẽ cố gắng làm thêm 1 bộ sinh Random board offline để nó load nhanh hơn, chứ API chạy đợi 15 giây mới chơi được thì toang :))");
